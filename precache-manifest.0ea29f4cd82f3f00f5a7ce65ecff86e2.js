a50f7fc412a859af46a2855b6509fb340a4b068b
folkaras <efolkaras@gmail.com>
2020-06-26 14:22:41 +0300

    Updates

---
+++ precache-manifest.0ea29f4cd82f3f00f5a7ce65ecff86e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffb43b27a77af7fbafaa33a1a04f2d3",
+    "url": "/bks-calculator/index.html"
+  },
+  {
+    "revision": "6a3f73877034ba8fc599",
+    "url": "/bks-calculator/static/css/main.e605e8f9.chunk.css"
+  },
+  {
+    "revision": "f9acff385283ce75c3a0",
+    "url": "/bks-calculator/static/js/2.b9dcf4aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bks-calculator/static/js/2.b9dcf4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3f73877034ba8fc599",
+    "url": "/bks-calculator/static/js/main.cf2bde84.chunk.js"
+  },
+  {
+    "revision": "3f969d201b08b73d933f",
+    "url": "/bks-calculator/static/js/runtime-main.a9268cb8.js"
+  },
+  {
+    "revision": "3c6a7d3ae76a5aec78aac69db1af15cf",
+    "url": "/bks-calculator/static/media/Geometria.3c6a7d3a.woff"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/bks-calculator/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "778525f6405ea0daa98d885a4ba17189",
+    "url": "/bks-calculator/static/media/Geometria.778525f6.eot"
+  },
+  {
+    "revision": "211c6b12eeb0d99d80fd50eae7d44e8e",
+    "url": "/bks-calculator/static/media/Shape.211c6b12.svg"
+  },
+  {
+    "revision": "6db9394252b5f2795684e12fdf93fb39",
+    "url": "/bks-calculator/static/media/cny.6db93942.svg"
+  },
+  {
+    "revision": "3d792df168e5b0103adc426560ab5b17",
+    "url": "/bks-calculator/static/media/eur.3d792df1.svg"
+  },
+  {
+    "revision": "21acd9777f3a4ce4b55becf06aedc20e",
+    "url": "/bks-calculator/static/media/gpb.21acd977.svg"
+  },
+  {
+    "revision": "1252216371cdd2cb81b3ec128321b0d7",
+    "url": "/bks-calculator/static/media/jpn.12522163.svg"
+  },
+  {
+    "revision": "2fd319cfef221202cdd6f275572cd983",
+    "url": "/bks-calculator/static/media/kaz.2fd319cf.svg"
+  },
+  {
+    "revision": "9f49afa6ca84d9ebd8a2a0fae6f6c294",
+    "url": "/bks-calculator/static/media/try.9f49afa6.svg"
+  },
+  {
+    "revision": "4f7685c1b080e7050ababaeb98e999ff",
+    "url": "/bks-calculator/static/media/usd.4f7685c1.svg"
+  }
+]);
